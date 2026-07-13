b650fb0b6d94aac94238c2f8c65918f1181ce1c3
Yuvraj Singh <102530074+yuvisingh9530@users.noreply.github.com>
2026-07-13 11:57:05 +0530

    Time: 1 ms (21.39%), Space: 12.4 MB (15.18%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        if n <= 0:
+            return False
+        while n % 4 == 0:
+            n //= 4
+        return n == 1
+        
